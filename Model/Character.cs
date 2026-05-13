9a8917972d666a841a0c9575601dee2299f39fa0
Kaowpan-second <pratchaya.cams1@gmail.com>
2026-05-13 22:21:16 +0700

    Add files via upload

---
+++ Model/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TurnBase.Model;
+
+namespace TurnBase.Model
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int MaxHP { get; set; }
+        public int CurrentHP { get; set; }
+        public int Attack { get; set; }
+        public int MP { get; set; }
+        public int MaxMP { get; set; }
+        public int Level { get; set; } = 1;
+        public int EXP { get; set; } = 0;
+        public int StatPoints { get; set; } = 0;
+        public bool IsAlive => CurrentHP > 0;
+        public bool IsGuarding { get; set; } = false;
+        public int Potions { get; set; } = 3;
+        public int Ethers { get; set; } = 2;
+        public string Sprite { get; set; }
+        public int BattleNumber { get; set; } = 1;
+        public List<string> StatusEffects { get; set; } = new();
+       
+    }
+}
---
+++ Model/CharacterClass.cs
@@ -0,0 +1,11 @@
+namespace TurnBase.Model;
+
+
+public class CharacterClass
+ {
+   public string Name { get; set; }
+   public int MaxHP { get; set; }
+   public int Attack { get; set; }
+   public int MP { get; set; }
+   public string Sprite { get; set; }
+}
